a414bb08101c2049fa0694aed56ef4f6c9854a4b
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-26 12:20:56 +0600

    added order page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './Componnent/Home/Home'
 import CheckoutPage from './Componnent/CheckoutPage/CheckoutPage'
+import OrserPage from './Componnent/Order/OrserPage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route index element={<Home></Home>}></Route>
       <Route path='checkout' element={<CheckoutPage></CheckoutPage>}></Route>
+      <Route path='orders' element={<OrserPage></OrserPage>}></Route>
     </Routes>
     
     </>
