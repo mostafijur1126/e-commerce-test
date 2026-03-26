7f47559ac39c319e9208efdc5158a4c537c64835
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-03-26 12:25:31 +0600

    added tracktin page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './Componnent/Home/Home'
 import CheckoutPage from './Componnent/CheckoutPage/CheckoutPage'
 import OrserPage from './Componnent/Order/OrserPage'
+import TrackingPage from './Componnent/TrackingPage/TrackingPage'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route index element={<Home></Home>}></Route>
       <Route path='checkout' element={<CheckoutPage></CheckoutPage>}></Route>
       <Route path='orders' element={<OrserPage></OrserPage>}></Route>
+      <Route path='tracking' element={<TrackingPage></TrackingPage>}></Route>
     </Routes>
     
     </>
